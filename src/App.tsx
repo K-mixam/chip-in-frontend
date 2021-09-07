e88d2a0dc6e3e4ee4f0d55a2a5e1af76bdb36543
iluxandr0 <inik3004@gmail.com>
2021-09-07 17:33:18 +0500

    rewrite project to typescript

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { FC } from "react";
+import {
+  useAdaptivity,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  ViewWidth,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell,
+} from "@vkontakte/vkui";
+import "@vkontakte/vkui/dist/vkui.css";
+
+const App: FC = () => {
+  const { viewWidth } = useAdaptivity();
+
+  return (
+    <AppRoot>
+      <SplitLayout header={<PanelHeader separator={false} />}>
+        <SplitCol spaced={viewWidth && viewWidth > ViewWidth.MOBILE}>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VKUI</PanelHeader>
+              <Group header={<Header mode="secondary">Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
+  );
+};
+
+export default App;
